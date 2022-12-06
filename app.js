a82a4180e23a461c356af206ed177d387253e24a
Axel <axelsp050@gmail.com>
2022-12-06 16:33:28 -0500

    added a hamburger menu function, another css tab to work on the home page, and some more design to the page, including a nav bar

---
+++ app.js
@@ -0,0 +1,17 @@
+// handles the actions of the header(nav bar)   
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '-1.5%';
+}
+
+function close() {
+    mainMenu.style.top = '-110%';
+}
